33e3a11601623574803bf74ac96ff822f32fed52
Jin Wang <jinw1990@gmail.com>
2016-02-17 20:52:37 +1100

    Initial Commit

---
+++ iSwift/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iSwift
+//
+//  Created by Jin Wang on 17/02/2016.
+//  Copyright © 2016 Uthoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
